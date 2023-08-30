f871b19d38a3c600b0e5904944097247172852f5
Jeremiah Greene <jerrygreene010@gmail.com>
2023-08-30 13:57:21 +0000

    added local files

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# @app.route('/whoami')
+# def whoami():
+#     return 
+    
+
+if __name__ == '__main__':
+    app.run(debug=True, host='127.0.0.1')
